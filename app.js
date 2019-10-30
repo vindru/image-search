57dbae25636aaa49788a512b1f153ff9b8810d10
vindru <rajatvindr00@gmail.com>
2019-10-30 16:45:48 +0530

    first commit

--- app.js
+++ app.js
@@ -22,8 +22,8 @@ app.get('/', function (req, res) {
     });
 })
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var server_port = process.env.OPENSHIFT_NODEJS_PORT
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP
  
 app.listen(server_port, server_ip_address, function () {
   console.log( "Listening on " + server_ip_address + ", port " + server_port )
